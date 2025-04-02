a933a6a2f92791009e42fa074e76c47df394c39b
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-02 22:56:55 +0200

    Add dynamic data to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-function Header() {
+import reactLogo from './assets/react-core-concepts.png';
+
+const reactDescriptions: string[] = [
+  'Core',
+  'Crucial',
+  'Fundamental',
+];
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function Header({}) {
+  const description: string = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+  
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactLogo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        { description } React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
