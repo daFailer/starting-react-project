3f48702785a46ade46b6609e74a342310f4dcb23
Michael Vogtherr <michael.vogtherr@check24.de>
2025-04-03 09:04:48 +0200

    Split up logic and outsource Header component into separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
-import reactLogo from './assets/react-core-concepts.png';
+import Header from './components/Header';
 import CoreConcept from './components/CoreConcept';
-const reactDescriptions: string[] = [
-  'Core',
-  'Crucial',
-  'Fundamental',
-];
-
-function getRandomInt(max: number) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header({}) {
-  const description: string = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
-  
-  return (
-    <header>
-      <img src={reactLogo} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        { description } React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
 function App() {
   return (
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { getRandomInt } from '../numbers';
+import reactLogo from '../assets/react-core-concepts.png';
+
+const reactDescriptions: string[] = [
+  'Core',
+  'Crucial',
+  'Fundamental',
+];
+
+export default function Header() {
+  const description: string = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+  
+  return (
+    <header>
+      <img src={reactLogo} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        { description } React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
